9f2d9845278c74a70e6c2c6fa0dfd91a178915bc
XenoBino <48479476+XenoBino@users.noreply.github.com>
2022-09-12 23:27:10 +0300

    feat: Patched keycode

--- config.def.h
+++ config.def.h
@@ -37,31 +37,31 @@ static Bool npisrelative  = False;
 #define MODKEY ControlMask
 static Key keys[] = {
 	/* modifier             key        function     argument */
-	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
-	{ MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
+	{ MODKEY|ShiftMask,     36,        focusonce,   { 0 } },
+	{ MODKEY|ShiftMask,     36,        spawn,       { 0 } },
 
-	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
-	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
+	{ MODKEY|ShiftMask,     46,        rotate,      { .i = +1 } },
+	{ MODKEY|ShiftMask,     43,        rotate,      { .i = -1 } },
+	{ MODKEY|ShiftMask,     44,        movetab,     { .i = -1 } },
+	{ MODKEY|ShiftMask,     45,        movetab,     { .i = +1 } },
+	{ MODKEY,               23,        rotate,      { .i = 0 } },
 
-	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
-	{ MODKEY,               XK_1,      move,        { .i = 0 } },
-	{ MODKEY,               XK_2,      move,        { .i = 1 } },
-	{ MODKEY,               XK_3,      move,        { .i = 2 } },
-	{ MODKEY,               XK_4,      move,        { .i = 3 } },
-	{ MODKEY,               XK_5,      move,        { .i = 4 } },
-	{ MODKEY,               XK_6,      move,        { .i = 5 } },
-	{ MODKEY,               XK_7,      move,        { .i = 6 } },
-	{ MODKEY,               XK_8,      move,        { .i = 7 } },
-	{ MODKEY,               XK_9,      move,        { .i = 8 } },
-	{ MODKEY,               XK_0,      move,        { .i = 9 } },
+	{ MODKEY,               49,        spawn,       SETPROP("_TABBED_SELECT_TAB") },
+	{ MODKEY,               10,        move,        { .i = 0 } },
+	{ MODKEY,               11,        move,        { .i = 1 } },
+	{ MODKEY,               12,        move,        { .i = 2 } },
+	{ MODKEY,               13,        move,        { .i = 3 } },
+	{ MODKEY,               14,        move,        { .i = 4 } },
+	{ MODKEY,               15,        move,        { .i = 5 } },
+	{ MODKEY,               16,        move,        { .i = 6 } },
+	{ MODKEY,               17,        move,        { .i = 7 } },
+	{ MODKEY,               18,        move,        { .i = 8 } },
+	{ MODKEY,               19,        move,        { .i = 9 } },
 
-	{ MODKEY,               XK_q,      killclient,  { 0 } },
+	{ MODKEY,               24,        killclient,  { 0 } },
 
-	{ MODKEY,               XK_u,      focusurgent, { 0 } },
-	{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
+	{ MODKEY,               30,        focusurgent, { .v = NULL } },
+	{ MODKEY|ShiftMask,     30,        toggle,      { .v = (void*) &urgentswitch } },
 
-	{ 0,                    XK_F11,    fullscreen,  { 0 } },
+	{ 0,                    95,        fullscreen,  { 0 } },
 };
